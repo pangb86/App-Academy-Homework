fc68390ddbd25e9caeda2c8adc31ea33785ae608
Bo <pangb86@gmail.com>
2017-10-26 12:57:32 -0700

    Add W4D4 homework

---
+++ W4D3/AuthHW/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+
+  before_validation :enusre_session_token
+
+  validates :username, :session_token, presence: true, uniqueness:true
+  validates :password_digest :presence => {message: "Password can't be blank"}
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user &&
+      BCrypt::Password.new(user.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom:urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token;
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+end
